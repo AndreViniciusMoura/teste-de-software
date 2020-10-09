d28f668fc55060746c6547f59c447bc6ea9a8f4e
andre.vinicius.moura@outlook.com <andre.vinicius.moura@outlook.com>
2020-10-09 20:55:27 -0300

    feat: criando o Traits

---
+++ 01 - Testes de Unidade/Features.Tests/01 - Traits/ClienteTests.cs
@@ -0,0 +1,53 @@
+﻿using Features.Clientes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Features.Tests._01___Traits
+{
+    public class ClienteTests
+    {
+        [Fact(DisplayName ="Novo Ciente válido")]
+        [Trait("Categoria", "Cliente Trait Testes")]
+        public void Cliente_NovoCliente_DeveEstarValido()
+        {
+            // Arrange
+            var cliente = new Cliente(Guid.NewGuid(),
+                                      "André",
+                                      "Moura",
+                                      DateTime.Now.AddYears(-30),
+                                      "andre.moura@email.com",
+                                      true,
+                                      DateTime.Now);
+
+            // Act
+            var result = cliente.EhValido();
+
+            // Assert
+            Assert.True(result);
+            Assert.Equal(0, cliente.ValidationResult.Errors.Count);
+        }
+
+        [Fact(DisplayName = "Novo Ciente inválido")]
+        [Trait("Categoria", "Cliente Trait Testes")]
+        public void Cliente_NovoCliente_DeveEstarInvalido()
+        {
+            // Arrange
+            var cliente = new Cliente(Guid.NewGuid(),
+                                      "",
+                                      "",
+                                      DateTime.Now,
+                                      "andre.moura@email.com",
+                                      true,
+                                      DateTime.Now);
+
+            // Act
+            var result = cliente.EhValido();
+
+            // Assert
+            Assert.False(result);
+            Assert.NotEqual(0, cliente.ValidationResult.Errors.Count);
+        }
+    }
+}
